64f42e167da0819ff00abb09df71250b1b3bac45
developersajeeb <sajeebdeveloper@gmail.com>
2025-07-07 21:43:58 +0600

    update some ui

--- src/features/api/apiSlice.ts
+++ src/features/api/apiSlice.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const apiSlice = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-system-server-a3.vercel.app/api' }),
   tagTypes: ['Book', 'Borrow', 'BorrowSummary'],
   endpoints: () => ({}),
 });
